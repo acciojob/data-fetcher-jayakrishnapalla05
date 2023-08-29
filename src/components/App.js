b2d9f59580bf655561bcee175006f43f117f80ec
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-29 15:07:19 +0530

    submitting code-Tue Aug 29 2023 15:07:18 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,43 @@
 
-import React ,{useState,useEffect} from "react";
-import './../styles/App.css';
-import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [data,setData]=useState([]);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-
-  function fetchData(){
+  useEffect(() => {
+    setLoading(true);
     fetch("https://dummyjson.com/products")
-    .then((response)=>response.json())
-    .then((response)=>{setData(response)})
-    .catch((err)=>{console.log(err);
-            setData(`An error occured: ${err}`)
-    }
-    )
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
-  useEffect(()=>{
-    fetchData();
-  },[]);
-  
-  if(!data)
-  {
-    return <p>Loading product data ....</p>;
+  if (error) {
+    return <div>An error occurred: {error.message}</div>;
   }
 
-
+  if (!data) {
+    return <div>No data found</div>;
+  }
 
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h1>Data Fetched from API</h1>
-        <pre className="data">{JSON.stringify(data,null,2)}</pre>
+      <h1>Data Fetched from API</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
