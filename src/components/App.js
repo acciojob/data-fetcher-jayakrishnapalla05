2b8e1c93c0695c7f8104f610551d373d04af1f38
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-29 15:02:16 +0530

    submitting code-Tue Aug 29 2023 15:02:15 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
-import React from "react";
+import React ,{useState,useEffect} from "react";
 import './../styles/App.css';
+import axios from "axios";
 
 const App = () => {
+  const [data,setData]=useState([]);
+
+
+  function fetchData(){
+    fetch("https://dummyjson.com/products")
+    .then((response)=>response.json())
+    .then((response)=>{setData(response)})
+    .catch((err)=>{console.log(err);
+            setData(`An error occured: ${err}`)
+    }
+    )
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[]);
+  
+  if(!data)
+  {
+    return <p>Loading product data ....</p>;
+  }
+
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Data Fetched from API</h1>
+        <pre className="data">{JSON.stringify(data,null,2)}</pre>
     </div>
   )
 }
